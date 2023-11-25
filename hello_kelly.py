f00582ded55b30612f85f6135fd5974329d5d445
kellybryant770 <kelly.bryant@reconni.com>
2023-11-25 12:42:53 -0500

    committing changes

--- hello_kelly.py
+++ hello_kelly.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_kelly():
-    return '<html><body><h1>Hello, Kelly!</h1></body></html>'
+    return '<html><body><h1>Hello, Kelly, way to go dude today!</h1></body></html>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
